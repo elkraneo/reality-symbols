1a23c6de5704a76148aab5d3d1497ff4e7f1f4cb
Cristian Díaz <cristian.diaz@monstar-lab.com>
2023-09-23 01:36:08 +0200

    wip

--- Sources/RealitySymbols/Entity+Type.swift
+++ Sources/RealitySymbols/Entity+Type.swift
@@ -1,6 +1,50 @@
 import Foundation
+import RealityKit
 
-#if os(visionOS)
+#if os(iOS)
+
+public enum EntityType: CaseIterable, Codable {
+    case entity
+    case modelEntity
+    case perspectiveCamera
+}
+
+extension EntityType {
+  public var type: RealityKit.Entity.Type {
+    switch self {
+    case .entity:
+      return Entity.self
+    case .modelEntity:
+      return ModelEntity.self
+    case .perspectiveCamera:
+      return PerspectiveCamera.self
+    }
+  }
+}
+
+#elseif os(macOS)
+
+public enum EntityType: CaseIterable, Codable {
+    case entity
+    case modelEntity
+    case perspectiveCamera
+}
+
+extension EntityType {
+  public var type: RealityKit.Entity.Type {
+    switch self {
+    case .entity:
+      return Entity.self
+    case .modelEntity:
+      return ModelEntity.self
+    case .perspectiveCamera:
+      return PerspectiveCamera.self
+    }
+  }
+}
+
+#elseif os(visionOS)
+
 public enum EntityType: CaseIterable, Codable {
     case anchorEntity
     case entity
@@ -8,4 +52,22 @@ public enum EntityType: CaseIterable, Codable {
     case perspectiveCamera
     case triggerVolume
 }
+
+extension EntityType {
+  public var type: RealityKit.Entity.Type {
+    switch self {
+    case .anchorEntity:
+      return AnchorEntity.self
+    case .entity:
+      return Entity.self
+    case .modelEntity:
+      return ModelEntity.self
+    case .perspectiveCamera:
+      return PerspectiveCamera.self
+    case .triggerVolume:
+      return TriggerVolume.self
+    }
+  }
+}
+
 #endif
