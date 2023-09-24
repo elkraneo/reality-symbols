85830b887ad7207d7f4d0671f5da2c73f99a2c1d
Cristian Díaz <cristian.diaz@monstar-lab.com>
2023-09-24 23:04:09 +0200

    feature: complete system image extension

--- Sources/RealitySymbols/EntityType+SystemImage.swift
+++ Sources/RealitySymbols/EntityType+SystemImage.swift
@@ -2,75 +2,73 @@ import Foundation
 
 #if os(iOS)
 
-extension EntityType {
-  public var systemImage: String {
-    switch self {
-    case .entity:
-      return "move.3d"
-    case .modelEntity:
-      return "cube"
-    case .perspectiveCamera:
-      return "camera"
+  extension EntityType {
+    public var systemImage: String {
+      switch self {
+        case .anchorEntity:
+          return "arrow.down.to.line"
+        case .bodyTrackedEntity:
+          return "figure.walk"
+        case .directionalLight:
+          return "sun.max"
+        case .entity:
+          return "move.3d"
+        case .modelEntity:
+          return "cube"
+        case .perspectiveCamera:
+          return "camera"
+        case .pointLight:
+          return "lightbulb.led"
+        case .spotLight:
+          return "lamp.desk"
+        case .triggerVolume:
+          return "cube.transparent"
+      }
     }
   }
-}
 
 #elseif os(macOS)
 
-extension EntityType {
-  public var systemImage: String {
-    switch self {
-    case .entity:
-      return "move.3d"
-    case .modelEntity:
-      return "cube"
-    case .perspectiveCamera:
-      return "camera"
+  extension EntityType {
+    public var systemImage: String {
+      switch self {
+        case .anchorEntity:
+          return "arrow.down.to.line"
+        case .directionalLight:
+          return "sun.max"
+        case .entity:
+          return "move.3d"
+        case .modelEntity:
+          return "cube"
+        case .perspectiveCamera:
+          return "camera"
+        case .pointLight:
+          return "lightbulb.led"
+        case .spotLight:
+          return "lamp.desk"
+        case .triggerVolume:
+          return "cube.transparent"
+      }
     }
   }
-}
 
 #elseif os(visionOS)
 
-extension EntityType {
-  public var systemImage: String {
-    switch self {
-    case .anchorEntity:
-      return "arrow.down.to.line"
-    case .entity:
-      return "move.3d"
-    case .modelEntity:
-      return "cube"
-    case .perspectiveCamera:
-      return "camera"
-    case .triggerVolume:
-      return "cube.transparent"
+  extension EntityType {
+    public var systemImage: String {
+      switch self {
+        case .anchorEntity:
+          return "arrow.down.to.line"
+        case .entity:
+          return "move.3d"
+        case .modelEntity:
+          return "cube"
+        case .perspectiveCamera:
+          return "camera"
+        case .triggerVolume:
+          return "cube.transparent"
+      }
     }
   }
-}
 
 #endif
-//extension CodableEntity.EntityType {
-//  public var symbol: String {
-//    switch self {
-//      case .anchor:
-//        return "arrow.down.to.line"
-//      case .bodyTrackedEntity:
-//        return "figure.walk"
-//      case .directionalLight:
-//        return "sun.max"
-//      case .entity:
-//        return "move.3d"
-//      case .model:
-//        return "cube"
-//      case .perspectiveCamera:
-//        return "camera"
-//      case .pointLight:
-//        return "lightbulb.led"
-//      case .spotLight:
-//        return "lamp.desk"
-//      case .triggerVolume:
-//        return "cube.transparent"
-//    }
-//  }
-//}
