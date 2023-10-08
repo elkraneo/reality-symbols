c1e98b9eba92a42c05b7af11ec6d5e831790d854
Cristian Díaz <elkraneo@cristians-mbp.speedport.ip>
2023-10-08 13:39:36 +0200

    wip

--- Sources/RealitySymbols/autogenerated/Component+Type.swift
+++ Sources/RealitySymbols/autogenerated/Component+Type.swift
@@ -181,7 +181,11 @@ extension ComponentType: CustomStringConvertible {
     public var type: RealityKit.Component.Type {
       switch self {
         case .accessibilityComponent:
-          return AccessibilityComponent.self
+          if #available(macOS 14.0, iOS 17.0, *) {
+            return AccessibilityComponent.self
+          } else {
+            fatalError()
+          }
         case .anchoringComponent:
           return AnchoringComponent.self
         case .characterControllerComponent:
